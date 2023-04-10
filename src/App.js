68f02f989aa98d7449cbfef9e37065e95b245b08
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2023-04-10 13:17:02 +0600

    Data show

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import DATA from "./assets/data/va.json";
+import { useState } from "react";
 
 function App() {
+  const [data, SetData] = useState(DATA);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table border="1">
+        <thead>
+          <tr>
+            <th>SL</th>
+            <th>Sale Date</th>
+            <th>Name</th>
+            <th>Card</th>
+            <th>Mobile</th>
+            <th>Model</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => {
+            console.log(item);
+            return (
+              <tr key={item.sl}>
+                <td>{item.sl}</td>
+                <td>{item.salesdate}</td>
+                <td>{item.name}</td>
+                <td>{item.card}</td>
+                <td>{item.mobile}</td>
+                <td>{item.model}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
